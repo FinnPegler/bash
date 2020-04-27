fb8e56570c4b22df1a4e41fac2d884b524061f1b
FinnPegler <finnpegler@gmail.com>
2020-04-27 17:41:04 +0100

    fixed server issue

--- assets/client2.js
+++ assets/client2.js
@@ -1,4 +1,4 @@
-var socket = io.connect("ec2-52-14-250-9.us-east-2.compute.amazonaws.com:3000");
+var socket = io.connect("ec2-18-191-142-129.us-east-2.compute.amazonaws.com:3000");
 let stage1 = 1;
 let stage2 = 1;
 let arr2 = [];
